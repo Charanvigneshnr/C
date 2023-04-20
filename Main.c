d931629468a1212ba57187c3cc2371be19b80b66
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-21 00:33:50 +0530

    structures cont..

--- Main.c
+++ Main.c
@@ -11,13 +11,24 @@ struct student
 int main()
 {
     system("cls");
-    struct student s;
-    int id1;
-    float cgpa1;
-    char name1[100];
-    id1 = s.id = 21;
-    cgpa1 = s.cgpa = 8.768;
-    strcpy(s.name, "Charan Vignesh");
-    strcpy(name1, s.name);
-    printf("Student ID = %u\nStudent Name = %u\nStudent CGPA = %u\n", &id1, &name1, &cgpa1);
+    struct student s[100];
+    s[0].id = 1;
+    strcpy(s[0].name, "Charan");
+    s[0].cgpa = 8.768;
+
+    s[1].id = 2;
+    strcpy(s[1].name, "Vignesh");
+    s[1].cgpa = 8.768;
+
+    struct student s2 = {3, "N R ", 8.768};
+
+    // int i;
+    // for (i = 0; i < 3; i++)
+    // {
+    //     printf("Student %d\nName: %s\nID: %d\nCGPA: %.2f\n\n", i + 1, s[i].name, s[i].id, s[i].cgpa);
+    // }
+    printf("Name: %s\nID: %d\nCGPA: %.2f\n\n", s2.name, s2.id, s2.cgpa);
+    return 0;
 }
+
+// 8.47 HRS
