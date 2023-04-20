c5b0902b6cef982618f8b146238273d0328c76f3
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-21 00:25:08 +0530

    structures

--- Main.c
+++ Main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+struct student
+{
+    int id;
+    char name[50];
+    float cgpa;
+};
 
 int main()
 {
     system("cls");
-    int a = 20;
-    int *ptr = &a;
-    int x = *ptr;
-    int y = ptr;
-    printf("x = %d\n", x);
-    printf("y = %d\n", y);
-    return 0;
+    struct student s;
+    int id1;
+    float cgpa1;
+    char name1[100];
+    id1 = s.id = 21;
+    cgpa1 = s.cgpa = 8.768;
+    strcpy(s.name, "Charan Vignesh");
+    strcpy(name1, s.name);
+    printf("Student ID = %u\nStudent Name = %u\nStudent CGPA = %u\n", &id1, &name1, &cgpa1);
 }
