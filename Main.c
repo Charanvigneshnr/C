ebc8f2987085b73176adfe12bbb08f7e462c2f5c
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-21 13:13:46 +0530

    typedef

--- Main.c
+++ Main.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-struct student
+typedef struct student
 {
     int id;
     char name[50];
     float cgpa;
-};
+} paiyan;
 
 int main()
 {
     system("cls");
-    struct student s[100];
-    s[0].id = 1;
-    strcpy(s[0].name, "Charan");
-    s[0].cgpa = 8.768;
-
-    s[1].id = 2;
-    strcpy(s[1].name, "Vignesh");
-    s[1].cgpa = 8.768;
-
-    struct student s2 = {3, "N R ", 8.768};
-
-    // int i;
-    // for (i = 0; i < 3; i++)
-    // {
-    //     printf("Student %d\nName: %s\nID: %d\nCGPA: %.2f\n\n", i + 1, s[i].name, s[i].id, s[i].cgpa);
-    // }
-    printf("Name: %s\nID: %d\nCGPA: %.2f\n\n", s2.name, s2.id, s2.cgpa);
+    paiyan s = {21, "Charan Vignesh", 8.768};
+    paiyan *ptr = &s;
+    printf("%.2f", ptr->cgpa);
     return 0;
 }
-
-// 8.47 HRS
